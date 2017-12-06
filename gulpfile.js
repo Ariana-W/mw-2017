5e40b35b1768b058a7600df3d4a229f9b3ee05be
Ariana-W <ariana.waller@yahoo.com>
2017-12-05 19:35:32 -0500

    Adding new addins to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
+var minifycss = require('gulp-minify-css');
+var plumber = require('gulp-plumber');
+var notifier = require('node-notifier');
+var notify = require('gulp-notify');
+
 var gulp = require ('gulp');
 //Gulp Sass Plugin
 var sass = require ('gulp-sass');
@@ -6,9 +11,19 @@ var sass = require ('gulp-sass');
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
-        .pipe(sass())
+        .pipe(sass()
+                    .on("error", notify.onError({
+                        title: "Sass Error",
+                        message: "Error: <%= error.message %>"
+                    }))
+                )
+
+        .pipe(plumber())
+        .pipe(minifycss())
         .pipe(gulp.dest('assets/css'));
 });
+
+
 
 gulp.task('default', function() {
     gulp.watch(['src/scss/*', 'src/scss/**/*'], ['sass']);
