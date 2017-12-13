1f5e6381d9e26dfab0d88d6d1f12ba90ea3001ae
Ariana-W <ariana.waller@yahoo.com>
2017-12-13 14:03:51 -0500

    adding javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var plumber = require('gulp-plumber');
 var notifier = require('node-notifier');
 var notify = require('gulp-notify');
 var browserSync = require('browser-sync').create();
+var uglify =require('gulp-uglify');
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
@@ -32,4 +33,17 @@ gulp.task('browser-sync', function() {
 gulp.task('default', ['sass', 'browser-sync'], function() {
     gulp.watch(['src/scss/*', 'src/scss/**/*'], ['sass']);
     gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch(['src/javascript/*', 'src/javascript/**/*'], ['scripts']);
+});
+
+// Javascript Task
+gulp.task('scripts', function() {
+    return gulp.src('src/javascript/*.js')
+        .pipe(uglify())
+
+        .on("error", notify.onError({
+            title: "JS Error",
+            message: "Error: <%= error.message %>"
+        }))
+        .pipe(gulp.dest('assets/js'));
 });
